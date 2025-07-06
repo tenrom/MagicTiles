986b864574e1e4de6a649a2f776185cb717e71b5
tenrom <tenrom35@gmail.com>
2025-07-06 18:03:24 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 
+
 let rng=new Math.seedrandom('Hello World')
 function NewLine(){
     n=Math.round(rng()*3)+1
@@ -6,17 +7,24 @@ function NewLine(){
     if (n===Number(document.getElementsByClassName('tile')[document.getElementsByClassName('tile').length-1].getAttribute('column'))){
         document.getElementsByClassName('tile')[document.getElementsByClassName('tile').length-1].GrowUp()
     }else{
-        document.body.innerHTML+=`<game-tile column=${n}></game-tile>`
+        document.getElementById("Game").innerHTML+=`<game-tile column=${n}></game-tile>`
     }
 }
 
 
-let PxSpeed=10
 
 function vibrer(){
     if (window.navigator.vibrate){
         window.navigator.vibrate(50)
     }
 }
+
+
+
+
+
+// Play Pause document.getElementById('play-pause-button').click()
+// Reset document.getElementsByClassName('previous-button')[0].click()
+// Change Speed document.getElementsByTagName('video')[0].playbackRate=0.5 
 
 
---
+++ variables.js
@@ -0,0 +1,1 @@
+let PxSpeed=10
